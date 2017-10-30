2ab7c9e520d33f68bafa438935b13489853b4bb3
Johan Book <johanbook@hotmail.com>
2017-10-30 17:51:19 +0100

    Update tempData.cpp

--- code/tempData.cpp
+++ code/tempData.cpp
@@ -33,41 +33,11 @@ tempData::tempData(string path)
 		if(line[0] == '#')
 			continue;
 		
-		cout << line << "\t> "; // For testing
-		
-		// Parse line, assuming 3 arguments (data, time, temp)
-		vector<string> parsed = parse_string(line, ';', 3);
-	
-		// Parse date
-		vector<string> date = parse_string(parsed.at(0), '-', 3);
-
-		// Parse time	
-		vector<string> time = parse_string(parsed.at(1), ':', 3);
-
-		// TESTING
-		print_vector(date);
-		cout << ":: ";
-		print_vector(time);
-		cout << ":: " << parsed.at(2) << "\n";
+		data.push_back(new tempPoint(line));
 	}
 
 	// Close file (obviously)
 	file.close();
-}
-
-// Parses a string for some delimiter and returns vector with substrings
-vector<string> tempData::parse_string(string line, char delimiter, int maxargs)
-{
-	istringstream string_reader(line);	
-	vector<string> parsed;
-	for(int i = 0; i < maxargs && string_reader; i++)
-	{	
-		string substring;
-		if(!getline(string_reader, substring, delimiter))
-			break;
-		parsed.push_back(substring);
-	}
-	return parsed;
 }
 
 // Main method to test class
