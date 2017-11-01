e03247aa7ecc5f50052692f712d21103956d4fbd
Johan Book <johanbook@hotmail.com>
2017-11-01 13:58:58 +0100

    Delete treader_cpp.d

--- code/treader_cpp.d
+++
@@ -1,7 +0,0 @@
-
-# DO NOT DELETE
-
-./code/treader_cpp.so: /home/courseuser/mnxb01-project/./code/treader.h
-./code/treader_cpp.so: /home/courseuser/mnxb01-project/./code/tpoint.h
-./code/treader_cpp.so: /usr/include/root/cintdictversion.h /usr/include/root/RVersion.h
-treader_cpp__ROOTBUILDVERSION= 5.34/30
