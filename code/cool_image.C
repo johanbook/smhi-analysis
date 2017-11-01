5a2906fcac644367f1754cc4aad3b0786676836c
Erik <johanbook@hotmail.com>
2017-11-01 16:36:26 +0100

    Plots yearly average temp. over different locations

--- code/cool_image.C
+++ code/cool_image.C
@@ -1,14 +1,14 @@
 // cool_image.C
 // 
 // Johan Book
-// 2017-10-27
+// 2017-11-01
 //
 // Plots an image
 //
 
 // include C++ STL headers 
 #include <iostream>
-#include <fstream>
+#include <vector>
 #include "treader.h"
 #include "tpoint.h"
 
@@ -16,13 +16,21 @@ using namespace std;
 
 // ROOT library obejcts
 #include <TF1.h> // 1d function class
+#include <TGraphErrors.h> // TGraphErrors
 #include <TH1.h> // 1d histogram classes
 #include <TStyle.h>  // style object
 #include <TMath.h>   // math functions
+#include <TMultiGraph.h> // TMultiGraph
 #include <TCanvas.h> // canvas object
 
 // Read and analyze part
 void generate_image(std::string path);
+
+// Creates a graph
+TGraphErrors* createGraph(std::string path);
+
+// Standard deviation
+double std_dev(vector<double> data, double mean);
 
 ///////////////////////////////////////////////////////////////
 // Source Code
@@ -31,30 +39,83 @@ void generate_image(std::string path);
 // Reads data and generates
 void generate_image(std::string path) 
 {
-	treader* tr = new treader(path);
-	
-	// Create histogram
-	TH1D* hPhi = new TH1D("hV2", "Rooion; #varphi; Counts", 100, 0, 2*TMath::Pi());
+	string paths[] = {"data/Boras.csv", "data/Falsterbo.csv", "data/Lulea.csv", "data/Lund.csv"};	
 
-	// Read from file while there is data to read
-	while(tr->has_next())
-	{
-		// Cout the year
-		cout << tr->get_tpoint()->get_year() << "\n";
-	}
-	
+	TMultiGraph* mg = new TMultiGraph();
+	for(int i = 0; i < 1; i++)
+		mg->Add(createGraph(paths[i]));
 	
 	// Set ROOT drawing styles
 	gStyle->SetOptStat(1111);
 	gStyle->SetOptFit(1111);
 
 	// Create canvas for hPhi
-	TCanvas* c1 = new TCanvas("c1", "v2 canvas", 900, 600);
-	hPhi->SetMinimum(0);
-	hPhi->Draw();
+	TCanvas* c1 = new TCanvas("c1", "canvas", 900, 600);
+	//mg->SetMinimum(0);
+	//mg->SetFillColorAlpha(kBlue, 0.1);
+	mg->Draw("a4");
 
 	// Save canvas as a picture
 	c1->SaveAs("cool_image.png");
 }
 
+TGraphErrors* createGraph(std::string path)
+{
+	treader* tr = new treader(path);
 
+	vector<int> year;
+	vector<double> temperature;
+	vector<double> stddev;
+
+	// Read from file while there is data to read
+	int year1 = -1;
+	while(tr->has_next()){
+		int year2 = year1;
+		double tsum = 0;
+		int nentries = 0;
+		vector<double> entries;
+		while(tr->has_next() && year1 == year2)
+		{
+			tpoint* tp = tr->get_tpoint();
+			tsum += tp->get_temperature();
+			entries.push_back(tp->get_temperature());
+			nentries++;
+			year2 = tp->get_year();	
+			if(year1 == -1)
+				year1 = year2;
+		}
+		year.push_back(year1);
+		temperature.push_back(tsum/nentries);
+		stddev.push_back(std_dev(entries, tsum/nentries));
+
+		year1 = year2;
+		
+		cout << year1 << " " << tsum / nentries << "\n";
+	}
+	int n = year.size();
+	double x[n];
+	double xe[n];
+	double y[n];
+	double ye[n];
+	for(int i = 0; i < n; i++)
+	{
+		x[i] = year[i];
+		xe[i] = 0;
+		y[i] = temperature[i];
+		ye[i] = stddev[i];
+	}
+	TGraphErrors* graph = new TGraphErrors(n,x,y,xe,ye);
+	graph->SetFillColor(6);
+	graph->SetFillStyle(3005);
+	//graph->SetFillColorAlpha(kBlue, 0.1);
+	return graph;
+}
+
+// Standard deviation
+double std_dev(vector<double> data, double avg)
+{
+	double sum = 0;
+	for(size_t i = 0; i < data.size(); i++)
+		sum += (data[i] - avg)*(data[i] - avg);
+	return TMath::Sqrt(sum/(data.size()-1));
+}
